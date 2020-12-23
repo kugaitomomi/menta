dec78fa0eb19304cffd718a60fedaa8af7d95b8f
久貝智美 <tottomo7@gmail.com>
2020-12-24 06:15:43 +0900

    fix file

--- main.js
+++ main.js
@@ -2,29 +2,28 @@
 console.log("=========================");
 console.log("現在持っているタスク一覧");
 console.log("=========================");
-const list = [
-  {task:"机を片付ける",genre:"掃除"},
-  {task:"牛乳を買う",genre:"買い物"},
-  {task:"散歩する",genre:"運動"},
+const lists = [
+  { task: "机を片付ける", genre: "掃除" },
+  { task: "牛乳を買う", genre: "買い物" },
+  { task: "散歩する", genre: "運動" },
 ];
-  for (let i = 0; i < list.length; i++ ){
-    console.log(`${i} : [内容] ${list[i].task}、[ジャンル] ${list[i].genre}`);
-}
-var c = list.length;
-var a = prompt("タスクを入力してください");
-var b = prompt("ジャンルを入力してください");
-  if(a && b){
-    list[c] = {task:a,genre:b};
-  }
+lists.forEach((list, index) => {
+  console.log(`${index} : [内容] ${list.task}、[ジャンル] ${list.genre}`);
+})
+
+const task = prompt("タスクを入力してください");
+const genre = prompt("ジャンルを入力してください");
+lists.push({ task: task, genre: genre });
 
 function addList() {
   console.log("=========================");
   console.log("現在持っているタスク一覧");
   console.log("=========================");
-  for (let i = 0; i < list.length; i++) {
-    console.log(`${i} : [内容] ${list[i].task}、[ジャンル] ${list[i].genre}`);
-
-  }
+  lists.forEach(
+    function (list, index) {
+      console.log(`${index} : [内容] ${list.task}、[ジャンル] ${list.genre}`);
+    }
+  );
 }
 addList();
-  prompt("「確認、追加、削除、終了」の4つのいずれかを入力してください");
+prompt("「確認、追加、削除、終了」の4つのいずれかを入力してください");
